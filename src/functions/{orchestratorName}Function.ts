30953501ce2a385d932fda2fc9a2dc87b89b255d
srdennywird <146856607+srdennywird@users.noreply.github.com>
2023-10-05 11:03:33 -0600

    nuevo orquestador 2

---
+++ src/functions/{orchestratorName}Function.ts
@@ -0,0 +1,39 @@
+import { app, HttpHandler, HttpRequest, HttpResponse, InvocationContext } from '@azure/functions';
+import * as df from 'durable-functions';
+import { ActivityHandler, OrchestrationContext, OrchestrationHandler } from 'durable-functions';
+
+const activityName = 'ActividadDePrueba';
+
+const DurableOrchestratorTestOrchestrator: OrchestrationHandler = function* (context: OrchestrationContext) {
+    const outputs = [];
+    outputs.push(yield context.df.callActivity(activityName, 'Tokyo'));
+    outputs.push(yield context.df.callActivity(activityName, 'Seattle'));
+    outputs.push(yield context.df.callActivity(activityName, 'Cairo'));
+
+    return outputs;
+};
+df.app.orchestration('PruebaPipelinePushTotal', DurableOrchestratorTestOrchestrator);
+
+const DurableOrchestratorTest: ActivityHandler = (input: string): string => {
+    return `Hello, ${input}`;
+};
+df.app.activity(activityName, { handler: DurableOrchestratorTest });import { app, HttpHandler, HttpRequest, HttpResponse, InvocationContext } from '@azure/functions';
+import * as df from 'durable-functions';
+import { ActivityHandler, OrchestrationContext, OrchestrationHandler } from 'durable-functions';
+
+const activityName = 'ActividadDePrueba';
+
+const DurableOrchestratorTestOrchestrator: OrchestrationHandler = function* (context: OrchestrationContext) {
+    const outputs = [];
+    outputs.push(yield context.df.callActivity(activityName, 'Tokyo'));
+    outputs.push(yield context.df.callActivity(activityName, 'Seattle'));
+    outputs.push(yield context.df.callActivity(activityName, 'Cairo'));
+
+    return outputs;
+};
+df.app.orchestration('PruebaPipelinePushTotal', DurableOrchestratorTestOrchestrator);
+
+const DurableOrchestratorTest: ActivityHandler = (input: string): string => {
+    return `Hello, ${input}`;
+};
+df.app.activity(activityName, { handler: DurableOrchestratorTest });
